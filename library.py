7e767afc910627ff392041804f5b33e9ea6b871c
mechv0d <sergeireim06@yandex.ru>
2025-01-26 16:26:08 +0700

    sunday work commit

--- library.py
+++ library.py
@@ -27,7 +27,8 @@ class Client(BaseModel):
     # region Methods
     def __init__(self, **data: ty.Any):
         super().__init__(**data)
-        self.id = md5(self.__hash_str).hexdigest()
+        if self.id is None:
+            self.id = md5(self.__hash_str).hexdigest()
 
     @staticmethod
     def create_from_console():
@@ -71,7 +72,8 @@ class Book(BaseModel):
     # region Methods
     def __init__(self, **data: ty.Any):
         super().__init__(**data)
-        self.id = md5(self.__hash_str).hexdigest()
+        if self.id is None:
+            self.id = md5(self.__hash_str).hexdigest()
 
     @staticmethod
     def create_from_console():
@@ -116,9 +118,10 @@ class Library(BaseModel):
         def __init__(self, /, **data: ty.Any):
             super().__init__(**data)
 
-            self.id = md5(
-                (datetime.now().strftime('%Y-%m-%d %H:%M:%S') + self.client_id + self.book_id).encode('utf-8')) \
-                .hexdigest()
+            if self.id is None:
+                self.id = md5(
+                    (datetime.now().strftime('%Y-%m-%d %H:%M:%S') + self.client_id + self.book_id).encode('utf-8')) \
+                    .hexdigest()
 
     # endregion
 
@@ -128,10 +131,9 @@ class Library(BaseModel):
     name: str
     address: str
 
-    clients: list[Client] = []
-    books: list[BookInArchive] = []
-
-    books_requests: list[BookRequest] = []
+    clients: list[Client]
+    books: list[BookInArchive]
+    books_requests: list[BookRequest]
 
     # endregion
 
@@ -141,37 +143,33 @@ class Library(BaseModel):
     def __hash_str(self):
         return (datetime.now().strftime('%Y-%m-%d %H:%M:%S') + self.name + self.address).encode('utf-8')
 
-    @property
-    def expired_requests(self) -> list[BookRequest]:
-        rqs = list()
-        for req in self.books_requests:
-            if req.is_expired():
-                rqs.append(req)
-        return rqs
-
     # endregion
 
     # region Methods
 
     def __init__(self, **data: ty.Any):
+        # self.clients = []
+        # self.books = []
+        # self.books_requests = []
         super().__init__(**data)
-        self.id = md5(self.__hash_str).hexdigest()
+        if self.id is None:
+            self.id = md5(self.__hash_str).hexdigest()
 
     def find_client(self, hash_id: str) -> Client | None:
         for client in self.clients:
-            if client.id.startswith(hash_id) or client.id == hash_id:
+            if Library.hash_similar(client.id, hash_id):
                 return client
         return None
 
     def find_book(self, hash_id: str) -> Book | None:
-        for book, count in self.books:
-            if book.id.startswith(hash_id) or book.id == hash_id:
-                return book
+        for archive in self.books:
+            if Library.hash_similar(archive.book.id, hash_id):
+                return archive.book
         return None
 
     def find_request(self, hash_id: str) -> BookRequest | None:
         for req in self.books_requests:
-            if req.id.startswith(hash_id) or req.id == hash_id:
+            if Library.hash_similar(req.id, hash_id):
                 return req
         return None
 
@@ -185,14 +183,58 @@ class Library(BaseModel):
         return self.find_request(hash_id) is not None
 
     def add_client(self, other: Client):
-        if other is None:
+        if other is None or not isinstance(other, Client):
             return
         self.clients.append(other)
+        print(f'Клиент {other.surname} {other.name} {other.last_name} успешно добавлен!\n')
 
     def add_book(self, other: Book, count=1):
-        if other is None:
+        if other is None or not isinstance(other, Book):
             return
         self.books.append(self.BookInArchive(book=other, count=count))
+        print(f'Книга "{other.title}" успешно добавлена в количестве: {count}шт.\n')
+
+    def remove_client(self, hash_id: str):
+        target = None
+
+        for client in self.clients:
+            if Library.hash_similar(client.id, hash_id):
+                target = client
+                break
+
+        if target:
+            self.clients.remove(target)
+
+    def remove_book(self, hash_id: str):
+        target = None
+
+        for archive in self.books:
+            if Library.hash_similar(archive.book.id, hash_id):
+                target = archive
+                break
+
+        if target:
+            self.books.remove(target)
+
+    def remove_request(self, hash_id: str):
+        target = None
+
+        for req in self.books_requests:
+            if Library.hash_similar(req.id, hash_id):
+                target = req
+                break
+
+        if target:
+            client = self.find_client(target.client_id)
+            book = self.find_book(target.book_id)
+
+            self.add_book_count(target.book_id, 1)
+            self.books_requests.remove(target)
+
+            print(f'{client.name} {client.surname} успешно вернул книгу {book.title}\n'
+                  f'Книг теперь в наличии: {self.book_count(book.id)}\n')
+        else:
+            print('Случилась непредвиденная ошибка при удалении запроса!\n')
 
     def book_count(self, book_id: str) -> int:
         if not self.has_book(book_id):
@@ -210,9 +252,9 @@ class Library(BaseModel):
 
         other = self.find_book(book_id)
 
-        for book_list in self.books:
-            if book_list.book.id == other.id and book_list.count - value >= 0:
-                book_list.count += value
+        for archive in self.books:
+            if archive.book.id == other.id and archive.count - value >= 0:
+                archive.count += value
                 return
 
     def add_book_request(self, client_id: str, book_id: str, days_to_claim: int,
@@ -220,36 +262,54 @@ class Library(BaseModel):
         # region Checks
 
         if not self.has_client(client_id):
-            print('Ошибка! Неверный идентификатор клиента!')
+            print('Ошибка! Неверный идентификатор клиента!\n')
             return
 
         if not self.has_book(book_id):
-            print('Ошибка! Неверный идентификатор книги!')
+            print('Ошибка! Неверный идентификатор книги!\n')
             return
 
         if self.book_count(book_id) - 1 < 0:
-            print('Ошибка! Данной книги нет в библиотеке или все экземпляры уже выданы на руки!')
+            print('Ошибка! Данной книги нет в библиотеке или все экземпляры уже выданы на руки!\n')
 
         if days_to_claim < 0:
-            print(f'Ошибка! Неверное количество дней {days_to_claim}!')
+            print(f'Ошибка! Неверное количество дней {days_to_claim}!\n')
             return
 
         if start_date > date.today():
-            print(f'Ошибка! Неверная дата выдачи!')
+            print(f'Ошибка! Неверная дата выдачи!\n')
             return
 
         # endregion
 
-        self.add_book_count(book_id, -1)
+        client = self.find_client(client_id)
+        book = self.find_book(book_id)
+
+        self.add_book_count(book.id, -1)
 
         end_date = start_date + timedelta(days_to_claim)
         if end_date < start_date:
             end_date = date.today()
-            print('Дата сдачи книги меньше чем дата взятия. Срок сдачи книги установлен на сегодня!')
+            print('Дата сдачи книги меньше чем дата взятия. Срок сдачи книги установлен на сегодня!\n')
 
         request = self.BookRequest(id=None, client_id=client_id, book_id=book_id,
                                    start_date=start_date, end_date=end_date)
-        self.books_requests.append(request)
+        if request:
+            self.books_requests.append(request)
+
+            print(
+                f'Книга "{book.title}" успешно взята клиентом '
+                f'{client.name} {client.surname} до {end_date.strftime("%d.%m.%Y")}'
+                '\nДоп. информация:\n'
+                f'Клиент: {client.id}\n'
+                f'Книга: {book.id}\n'
+                f'Идентификатор запроса: {request.id}\n'
+                f'Книг осталось в наличии: {self.book_count(book.id)}\n')
+
+            return request
+        else:
+            print('Возникла непредвиденная ошибка при создании запроса!\n')
+            return None
 
     def dump_json(self):
         return self.model_dump_json(indent=2)
@@ -275,14 +335,127 @@ class Library(BaseModel):
             title = genre.title if genre else tag
             print(f'Книг в жанре "{title}" хранится: {count}')
 
+    def expired_requests(self) -> list[BookRequest]:
+        rqs = list()
+        for req in self.books_requests:
+            if req.is_expired():
+                rqs.append(req)
+        return rqs
+
+    @staticmethod
+    def hash_similar(hash_id: str, substr: str) -> bool:
+        return hash_id.startswith(substr) or hash_id == substr
+
+    @staticmethod
+    def __print_book(book_obj: Book, count: int, i=1):
+        print(f'''Книга №{i} ({count}шт.) {book_obj.id}
+"{book_obj.title}"
+— {book_obj.author}, {book_obj.publication_date}
+{GenreExtensions.find_by_tag(book_obj.genre_tag).title}
+ISBN {book_obj.isbn}
+                    ''')
+
+    @staticmethod
+    def __print_client(client_obj: Client, i=1):
+        print(f'''Клиент №{i} {client_obj.id}
+{client_obj.surname} {client_obj.name} {client_obj.last_name}
+{client_obj.phone_num}
+                            ''')
+
+    def __print_request(self, request_obj: BookRequest, i=1):
+        if request_obj is None:
+            return
+        client = self.find_client(request_obj.client_id)
+        book = self.find_book(request_obj.book_id)
+        print(f'''Запрос №{i} {request_obj.id}
+Клиент: {client.surname} {client.name} {client.last_name} ({client.id})
+Книга: "{book.title}", {book.author} ({book.id})
+Дата выдачи: {request_obj.start_date} до {request_obj.end_date}
+Просрочен: {"ДА" if request_obj.is_expired() else "-"}
+        ''')
+
+    def print_books(self, limit=50, title='', author='', publication_date='', isbn='', description='',
+                    genre_tag=''):
+        printed_count = 0
+        for archive in self.books:
+            i = self.books.index(archive) + 1
+            book = archive.book
+
+            if (title != '' and book.title.lower().count(title.lower()) < 1) \
+                    or (author != '' and book.author.lower().count(author.lower()) < 1) \
+                    or (publication_date != '' and book.publication_date.lower().count(publication_date.lower()) < 1) \
+                    or (isbn != '' and book.isbn.lower().count(isbn.lower()) < 1) \
+                    or (description != '' and book.description.lower().count(description.lower()) < 1) \
+                    or (genre_tag != '' and book.genre_tag.lower().count(genre_tag.lower()) < 1):
+                continue
+
+            printed_count += 1
+            Library.__print_book(book, archive.count, i)
+            if printed_count == limit:
+                break
+        if printed_count == 0:
+            print('По данному запросу не нашлось записей. Проверьте правильность написания')
+        print(f'>>> Найдено записей: {printed_count}.\n')
+
+    def print_clients(self, limit=50, surname='', name='', last_name='', phone_num=''):
+        printed_count = 0
+        for client in self.clients:
+            i = self.clients.index(client) + 1
+
+            if (surname != '' and client.surname.lower().count(surname.lower()) < 1) \
+                    or (name != '' and client.name.lower().count(name.lower()) < 1) \
+                    or (last_name != '' and client.last_name.lower().count(last_name.lower()) < 1) \
+                    or (phone_num != '' and client.phone_num.count(phone_num.lower()) < 1):
+                continue
+
+            printed_count += 1
+            Library.__print_client(client, i)
+            if printed_count == limit:
+                break
+        if printed_count == 0:
+            print('По данному запросу не нашлось записей. Проверьте правильность написания')
+        print(f'>>> Найдено записей: {printed_count}.\n')
+
+    def print_requests(self, limit=50, request_id='', client_id='', book_id='', only_expired=False):
+        printed_count = 0
+        for req in self.books_requests:
+            i = self.books_requests.index(req) + 1
+
+            if (request_id != '' and not Library.hash_similar(req.id, request_id)) \
+                    or (client_id != '' and not Library.hash_similar(req.client_id, client_id)) \
+                    or (book_id != '' and not Library.hash_similar(req.book_id, book_id)) \
+                    or (only_expired is not False and not req.is_expired()):
+                continue
+
+            printed_count += 1
+            self.__print_request(req, i)
+            if printed_count == limit:
+                break
+        if printed_count == 0:
+            print('По данному запросу не нашлось записей. Проверьте правильность написания')
+        print(f'>>> Найдено записей: {printed_count}.\n')
+
+        # endregion
+
     # endregion
 
 
 class Genre:
+    # region Variables
     tag: str
     title: str
     for_adults_only: bool
 
+    # endregion
+
+    # region Properties
+    @property
+    def dump_data(self) -> dict:
+        return {'tag': self.tag, 'title': self.title, 'for_adults_only': self.for_adults_only}
+
+    # endregion
+
+    # region Methods
     def __init__(self, tag: str, title: str, for_adults_only=False, add_itself=True):
         self.tag = tag
         self.title = title
@@ -290,20 +463,22 @@ class Genre:
         if add_itself and GenreExtensions.find_by_tag(tag) is None:
             GenreExtensions.genres.append(self)
 
-    @property
-    def dump_data(self) -> dict:
-        return {'tag': self.tag, 'title': self.title, 'for_adults_only': self.for_adults_only}
-
     def dump_json(self) -> str:
         try:
             return json.dumps(self.dump_data, indent=4, ensure_ascii=False)
         except TypeError:
             return "{}"
 
+    # endregion
+
 
 class GenreExtensions:
+    # region Variables
     genres: list[Genre] = []
 
+    # endregion
+
+    # region Methods
     @staticmethod
     def clear_repeats():
         GenreExtensions.genres = set(GenreExtensions.genres)
@@ -329,5 +504,21 @@ class GenreExtensions:
                 return loaded_genres
         except (FileNotFoundError, json.JSONDecodeError):
             return []
+    # endregion
 
+
+class ConstructorExtensions:
+    @staticmethod
+    def create_book(title: str, author: str, pages: int, publication_date: str, isbn: str, description: str,
+                    genre_tag: str) -> Book:
+        return Book(id=None, title=title, author=author, pages=pages, publication_date=publication_date, isbn=isbn,
+                    description=description, genre_tag=genre_tag)
+
+    @staticmethod
+    def create_client(surname: str, name: str, last_name: str, phone_num: str) -> Client:
+        return Client(id=None, surname=surname, name=name, last_name=last_name, phone_num=phone_num)
+
+    @staticmethod
+    def create_library(name: str, address: str) -> Library:
+        return Library(id=None, name=name, address=address, books=[], clients=[], books_requests=[])
 # endregion
--- main.py
+++ main.py
@@ -1,19 +1,101 @@
 from library import *
+from library import ConstructorExtensions as con
+import os
 
-genres = GenreExtensions.load_genres_from_json('genres.json', True)
 
-'''
-test_cl = Client(id=None, surname='Рем', name='Сергей', last_name='Борисович', phone_num='+79339334964')
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-test_b = Book(id=None, title='Жизнь', author='Я', pages=51,
-              publication_date='12.05.2006', isbn='AC951-8524-BVM', description='Эгоист.', genre_tag='autobiography')
-              
+
+GenreExtensions.load_genres_from_json('genres.json', True)
+genres = GenreExtensions.genres
+
+
+def write_to_file(filename, write_data):
+    try:
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(write_data)
+    except (IOError, OSError) as open_error:
+        print(f"Ошибка при работе с файлом: {open_error}")
+
+
 with open('test_lib.json', 'r', encoding='utf-8') as f:
     cont = f.read()
-    test_lib = Library.model_validate_json(cont)
+    try:
+        test_lib = Library.model_validate_json(cont)
+    except ValidationError as val_error:
+        print(f"Failed to load general library JSON file\n{val_error}")
+    else:
+        # Для более легкого создания экземпляров класса я написал специальный класс ConstructorExtensions,
+        # методы которого облегчают создание основных классов программы.
 
-test_lib.print_sorted_by_genre()
-'''
+        # Такой костыль вызван наследованием от класса BaseModel из библиотеки pydantic. Он позволяет легко сохранять
+        # и загружать данные в JSON, но имеет некоторые проблемы с __init__ методом дочернего класса.
+        # Даже так, классы всё равно можно создавать напрямую
 
+        # Создание второй библиотеки
+        other_lib = con.create_library('Вторая библиотека', 'ул. Пушкина, д. Колотушкина')
 
+        # Добавление книг
+        books = [con.create_book('Герой нашего времени', 'Лермонтов', 111, 'long time ago', 'None', 'test', ''),
+                 con.create_book(
+                     'Конституция Российской Федерации. Новая редакция (с комментариями Конституционного Суда РФ)',
+                     'Проспект', 120, '2022',
+                     '978-5-392-32914-4, 978-5-392-33207-6, 9785392343942, 978-5-392-35252-4, 978-5-392-36498-5, 978-5-392-37237-9 ',
+                     '', 'social')
+                 ]
+        other_lib.add_book(books[0], 25)
+        other_lib.add_book(books[1], 10)
 
+        # Добавление клиента
+        clients = [con.create_client('Трамп', 'Дональд', '', '+71234567879')]
+        other_lib.add_client(clients[0])
+
+        # Посмотрим, добавились ли они
+        other_lib.print_clients(name='нальд')
+        other_lib.print_books(title='конст')
+
+        # Кажется, Дональд собирается почитать интересную литературу
+        trump_request = other_lib.add_book_request(clients[0].id, books[1].id, 30)
+
+        # Он ошибся
+        other_lib.remove_request(trump_request.id)
+
+        # Он забыл, что уже вернул книгу, и решил снова попытаться сдать её
+        other_lib.remove_request(trump_request.id)
+
+        # Он позвал своего старого друга посидеть в библиотеке
+        clients.append(con.create_client('Байден', 'Джозеф', '', '+79139135566'))
+        other_lib.add_client(clients[1])
+
+        # Джо решил почитать классику
+        joe_request = other_lib.add_book_request(clients[1].id, books[0].id, 15)
+
+        save_input = input(
+            'Тестирование созданной библиотеки завершено. Если хотите, можете сохранить данные, вписав имя файла '
+            '(например, "other_lib.json"). Если не хотите, оставьте поле пустым."\n'
+            'После этого начнётся тестирование загруженной библиотеки из файла test_lib.json\n')
+
+        if not save_input == '':
+            write_data = other_lib.dump_json()
+            write_to_file(save_input, write_data)
+
+        clear()
+
+        # Вывести книги жанра "Антиутопия"
+        test_lib.print_books(genre_tag='dyst')
+
+        # Вывести клиентов
+        test_lib.print_clients()
+
+        # Вывести запросы у клиента, чей хеш начинается с '4a69' (это я)
+        test_lib.print_requests(client_id='4a69')
+
+        # Вывести только просроченные запросы
+        test_lib.print_requests(only_expired=True)
+
+        # Я извинился и вернул книгу
+        test_lib.remove_request('466d3')
+
+        # И запрос исчез
+        test_lib.print_requests(only_expired=True)
